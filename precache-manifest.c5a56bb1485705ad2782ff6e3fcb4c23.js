58cd0f1a0b99b413d0d9741f513b05d267a2fed3
arpit-krazybee <arpit.gaurav@krazybee.com>
2019-07-17 20:08:11 +0530

    Updates

---
+++ precache-manifest.c5a56bb1485705ad2782ff6e3fcb4c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df148724b825a3668dcc2b23cfe2da7",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "6cd1303ce92337c69992",
+    "url": "/counter-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d1cb2b6310b467f0bc00",
+    "url": "/counter-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6cd1303ce92337c69992",
+    "url": "/counter-app/static/js/2.a6bbc099.chunk.js"
+  },
+  {
+    "revision": "d1cb2b6310b467f0bc00",
+    "url": "/counter-app/static/js/main.af02db9e.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counter-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
