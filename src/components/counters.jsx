6af31ab8678dd6caaa53c8cf4d601e02abf67625
arpit-krazybee <arpit.gaurav@krazybee.com>
2019-07-18 11:25:40 +0530

    initial commit

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,10 +3,7 @@ import Counter from './counter';
 class Counters extends Component {
     state = { 
         counters:[
-            {id:1,value:1},
-            {id:2,value:1},
-            {id:3,value:1},
-            {id:4,value:1}
+            {id:1,value:1}
         ]
      };
 
